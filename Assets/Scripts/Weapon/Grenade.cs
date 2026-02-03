b1932d67c71a8a6936a0b62e79801ee5f62f0ad8
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-02 22:43:06 -0500

    Implement grenade explosion with physics force

---
+++ Assets/Scripts/Weapon/Grenade.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    private float countdown;
+    public float radius = 5f;
+    public float explosionForce = 70f;
+    bool exploted = false;
+    private void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !exploted) {
+            Explote();
+            exploted = true;
+        }
+    }
+
+    private void Explote() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var coll in colliders) {
+            Rigidbody rb = coll.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddExplosionForce(explosionForce *  10 , transform.position, radius);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+
+}
