2538a0e7b3fd3e83c56068f05b7a3f75ff47c03c
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-03 00:23:42 -0500

    Add granede weapon and update HUD

--- Assets/Scripts/Player/PlayerInteractions.cs
+++ Assets/Scripts/Player/PlayerInteractions.cs
@@ -6,6 +6,11 @@ public class PlayerInteractions : MonoBehaviour
     {
         if (other.gameObject.CompareTag("GunAmmo")) { 
             GameManager.Instance.gunAmmo += other.gameObject.GetComponent<AmmoBox>().ammoAmount;
+            // Actualizar HUD
+            GameManager.Instance.UpdateAmmoUI(
+                GameManager.Instance.gunAmmo,
+                GameManager.Instance.gunIcon
+            );
             Destroy(other.gameObject);
         }
     }
--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -3,8 +3,11 @@ using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
+    [Header("References")]
     public Transform spawnPoint;
     public GameObject bullet;
+
+    [Header("Gun Settings")]
     public float shotForce = 1500f;
     public float shotRate = 0.5f;
 
@@ -12,15 +15,59 @@ public class Gun : MonoBehaviour
 
     void Update()
     {
-        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) {
-            if (Time.time > shotRateTime && GameManager.Instance.gunAmmo > 0) {
-                GameManager.Instance.gunAmmo--;
-                GameObject newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
-                shotRateTime = Time.time + shotRate;
-                Destroy(newBullet, 5f); // Destroy bullet after 5 seconds
-            }
+        if (Mouse.current == null) return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            TryShoot();
         }
-        
     }
+
+    void TryShoot()
+    {
+        if (Time.time < shotRateTime) return;
+        if (GameManager.Instance.gunAmmo <= 0) return;
+
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        GameManager.Instance.gunAmmo--;
+
+        GameObject newBullet = Instantiate(
+            bullet,
+            spawnPoint.position,
+            spawnPoint.rotation
+        );
+
+        Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(spawnPoint.forward * shotForce);
+        }
+
+        shotRateTime = Time.time + shotRate;
+
+        Destroy(newBullet, 5f);
+
+        // Actualizar HUD
+        GameManager.Instance.UpdateAmmoUI(
+            GameManager.Instance.gunAmmo,
+            GameManager.Instance.gunIcon
+        );
+    }
+    //void Update()
+    //{
+    //    if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) {
+    //        if (Time.time > shotRateTime && GameManager.Instance.gunAmmo > 0) {
+    //            GameManager.Instance.gunAmmo--;
+    //            GameObject newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+    //            newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+    //            shotRateTime = Time.time + shotRate;
+    //            Destroy(newBullet, 5f); // Destroy bullet after 5 seconds
+    //        }
+    //    }
+
+    //}
 }
