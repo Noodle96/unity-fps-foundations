ee4f8e4354bc6cf406b15fb86c822b3dc0676dfc
Noodle96 <jorgealfredomarino2@gmail.com>
2026-02-02 21:56:39 -0500

    Update README with HUD system

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,12 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public int gunAmmo = 10;
+    public int playerHealth = 100;
+
+    public TextMeshProUGUI ammoText;
+    public TextMeshProUGUI healthText;
 
     private void Awake()
     {
             Instance = this;
+    }
+
+    private void Update()
+    {
+        ammoText.text = gunAmmo.ToString();
+        healthText.text = playerHealth.ToString();
     }
 }
